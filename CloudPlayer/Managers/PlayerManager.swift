0b7e0777a8bb84229111053119f9101d5ae0bdce
Tim <spridl@mail.ru>
2025-08-08 15:10:21 +0300

    add Player Manager

---
+++ CloudPlayer/Managers/PlayerManager.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerManager.swift
+//  CloudPlayer
+//
+//  Created by Тимур on 07.08.2025.
+//
+
+import Foundation
---
+++ CloudPlayer/RootViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RootViewController.swift
+//  CloudPlayer
+//
+//  Created by Тимур on 05.08.2025.
+//
+
+import Foundation
